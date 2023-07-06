ab70ba7f7c2d00e217522fd47f2f7cd877c5c0da
Abraham Samuel Adekunle <abrahamadekunle50@gmail.com>
2023-07-06 19:08:38 +0100

    Concatenate a string using annotations

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+    Desc:
+    Takes a two strings and returns a concatenated string
+    Args:
+        str1: string
+        str2: string
+    Return:
+        String
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    returns a concatenated string
+    """
+    return str1 + str2
