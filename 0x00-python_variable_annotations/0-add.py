7ec17e33d7236009b662c85b91f34469391ab184
Abraham Samuel Adekunle <abrahamadekunle50@gmail.com>
2023-07-06 17:45:05 +0000

    add basic annotations

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+A Module that contains a type-annotated function
+Args:
+    a: float
+    b: float
+Return: float
+"""
+
+def add(a: float, b:float) -> float:
+    return a + b
