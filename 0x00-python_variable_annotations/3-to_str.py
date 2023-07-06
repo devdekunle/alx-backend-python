77aa1429fdc6361b64b664356bbab68c89ef896e
Abraham Samuel Adekunle <abrahamadekunle50@gmail.com>
2023-07-06 19:21:52 +0100

    converts a float to a string

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,17 @@
+#!/usr/bin/rnv python3
+"""
+    Desc:
+        type-annotated function to_str that takes a float n as argument
+        and returns the string representation of the float.
+    Args:
+        n: float
+    Return:
+        string
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    converts a float to string and returns it
+    """
+    return str(n)
