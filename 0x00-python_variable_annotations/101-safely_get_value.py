240fbdb3f323c8f6ebe79dd66d385b9a1b25a209
Abraham Samuel Adekunle <abrahamadekunle50@gmail.com>
2023-07-07 09:15:29 +0100

    add function documentation

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -19,6 +19,9 @@ def safely_get_value(dct: Mapping,
                      key: Any,
                      default: Union[T, None] = None
                      ) -> Union[Any, T]:
+    """
+        returns any data type or TypeVar
+    """
     if key in dct:
         return dct[key]
     else:
