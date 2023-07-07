fbd21225e834591d5a83049f19ae637bf96dc395
Abraham Samuel Adekunle <abrahamadekunle50@gmail.com>
2023-07-07 04:21:02 +0100

    correct interpreter

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 """
     Desc:
          a type-annotated function to_kv that takes a string k
---
+++ 0x00-python_variable_annotations/8-make_multiply.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+    Desc:
+         a type-annotated function make_multiplier that takes a float
+         multiplier as argument and returns a function that
+         multiplies a float by multiplier.
+    Args:
+        multiplier: float
+    Return:
+        function: callable
+"""
+from typing import List, Tuple, Union, Callable
+
+
+def make_multiplier(multiplier: float) -> 
