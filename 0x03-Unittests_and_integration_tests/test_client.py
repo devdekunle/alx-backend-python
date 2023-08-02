50f0974a9e052e0598303ebaa40c1731ec3711d7
Abraham Samuel Adekunle <abrahamadekunle50@gmail.com>
2023-08-02 10:08:56 +0100

    parameterize and patch a method

---
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+"""
+Test cases for client.py
+"""
+import unittest
+from unittest.mock import patch, MagicMock
+import client
+from client import GithubOrgClient
+from parameterized import parameterized
+
+
+class TestGithubOrgClient(unittest.TestCase):
+    """
+    testcase for the get_json method for returning the json of a client
+    """
+    @parameterized.expand([
+        ("google", {"testing_google": "google"}),
+        ("abc", {"testing_abc": "abc"})
+    ])
+    @patch('client.get_json')
+    def test_org(self, org_name, json_result, mock_object):
+        """
+        test that org returns the correct value
+        """
+        mock_object.return_value = json_result
+
+        # create instance of GithubOrgClient
+        github_org = GithubOrgClient(org_name)
+        self.assertEqual(github_org.org, json_result)
+        url = github_org.ORG_URL.format(org=org_name)
+        mock_object.assert_called_once_with(url)
--- 0x03-Unittests_and_integration_tests/utils.py
+++ 0x03-Unittests_and_integration_tests/utils.py
@@ -69,6 +69,7 @@ def memoize(fn: Callable) -> Callable:
     def memoized(self):
         """"memoized wraps"""
         if not hasattr(self, attr_name):
+            print("setting attribute")
             setattr(self, attr_name, fn(self))
         return getattr(self, attr_name)
 
