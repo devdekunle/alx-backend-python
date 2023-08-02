b4f7a46574989ae68e3936b10e8e4c69305c1d47
Abraham Samuel Adekunle <abrahamadekunle50@gmail.com>
2023-08-02 15:51:04 +0100

    implement more patching of api methods

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -46,3 +46,30 @@ class TestGithubOrgClient(unittest.TestCase):
             self.assertEqual(github_org._public_repos_url,
                              json_result['repos_url'])
             mock_object.assert_called_once
+
+    @parameterized.expand([
+        ("google", [{"testing_google1": "google",
+                    "repos_url": "https://google.com",
+                    "other_details": "some_other things",
+                    "name": "truth"
+                    },
+                    {"testing_google2": "google",
+                    "repos_url": "https://google2.com",
+                    "name": "Apache api"}])
+    ])
+    @patch('client.get_json')
+    def test_public_repos(self, org_name, json_result, mock_object):
+        """
+        test the values of the public reponse
+        """
+        mock_object.return_value = json_result
+
+        with patch('client.GithubOrgClient._public_repos_url',
+                   new_callable=PropertyMock) as mock_response:
+            mock_response.return_value = "https://google.com"
+            github_obj = GithubOrgClient(org_name)
+            self.assertEqual(github_obj.public_repos(), ['truth', 'Apache api'])
+            mock_object.assert_called_once
+            mock_response.assert_called_once
+
+
