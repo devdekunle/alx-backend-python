65688d0cdcd68bbed9ba815aeff45d8c9172d64a
Abraham Samuel Adekunle <abrahamadekunle50@gmail.com>
2023-08-02 17:07:57 +0100

    implement more parameterization

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -73,3 +73,14 @@ class TestGithubOrgClient(unittest.TestCase):
                              ['truth', 'Apache api'])
             mock_object.assert_called_once()
             mock_response.assert_called_once()
+
+    @parameterized.expand([
+        ({"license": {"key": "my_license"}}, "my_license", True),
+        ({"license": {"key": "other_license"}}, "my_license", False),
+    ])
+    def test_has_license(self, repo, license_key, status):
+        """
+        test for has_license metho
+        """
+        obj = GithubOrgClient('google')
+        self.assertEqual(obj.has_license(repo, license_key), status)
